2acb5e724c3ea0e7b66ae845c40bac1b1958c6f7
Rashida A <rashida@raeccleston.com>
2022-03-10 19:59:02 -0500

    First attempt at installing Express server

--- index.js
+++ index.js
@@ -1,1 +1,80 @@
+//Create express server
+let express = require("express");
+let app = express();
+
+
+//Allow for JSON Parsing
+
+/*
+
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 8080;
+
+// Create HTTP server
+const server = http.createServer(function(req, res) {
+
+   // Set the response HTTP header with HTTP status and Content type
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+
+   // Send the response body "Hello World"
+   res.end('Hello World\n');
+});
+
+// Prints a log once the server starts listening
+server.listen(port, hostname, function() {
+   console.log(`Server running at http://${hostname}:${port}/`);
+})
+*/
+/*
 alert("Hello");
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+*/
+
+/*
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+*/
+
+/*
+
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('index.html').pipe(res)
+})
+
+server.listen(process.env.PORT || 3000);
+
+const requestListener = function (req, res) {
+    fs.readFile(__dirname + "/index.html")
+        .then(contents => {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(contents);
+        })
+        .catch(err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        });}
+
+        */
